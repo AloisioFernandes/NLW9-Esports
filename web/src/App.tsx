249c6b02d0dbaa220a60759ffbffd04dba70ed27
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-10-08 21:29:56 -0300

    Gradiente.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,7 +7,9 @@ function App() {
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
 
-      <h1>Seu duo está aqui.</h1>
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.
+      </h1>
     </div>
   )
 }
