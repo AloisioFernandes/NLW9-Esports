516365830c25ebadbb60c1da66b069f47d795444
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-10-21 20:57:46 -0300

    Componente GameCard.

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { View, Image } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png'
+import { GameCard } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
+
+import { GAMES } from '../../utils/games';
 
 import { styles } from './styles';
 
@@ -19,6 +22,9 @@ export function Home() {
         subtitle="Selecione o game que deseja jogar..."
       />
 
+      <GameCard 
+        data={GAMES[0]}
+      />
     </View>
   );
 }
