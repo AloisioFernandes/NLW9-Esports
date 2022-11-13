b882ef33d68a1c3ca706463cf2ef971021a76e7c
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-11-12 23:04:29 -0300

    Estilização inicial modal.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -54,13 +54,13 @@ function App() {
         <CreateAdBanner />
 
         <Dialog.Portal>
-          <Dialog.Overlay />
+          <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
 
-          <Dialog.Content>
+          <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px]">
             <Dialog.Title>Publique um anúncio</Dialog.Title>
 
             <Dialog.Content>
-              
+
             </Dialog.Content>
           </Dialog.Content>
         </Dialog.Portal>
