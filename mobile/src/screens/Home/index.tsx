7e6bf33e3d0c129a7276f3886c536929825b912e
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-11-24 20:55:38 -0300

    Iniciando navegação.

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -12,7 +12,7 @@ export function Home() {
   const [games, setGames] = useState<GameCardProps[]>([])
 
   useEffect(() => {
-    fetch('http://192.168.1.16:3333/games')
+    fetch('http://192.168.1.2:3333/games')
     .then(response => response.json())
     .then(data => setGames(data))
   }, [])
